1c8c38f9703fe748532f6543f7f1e006d0968542
ilknurboyrazli <ilknurboyrazli5@gmail.com>
2026-09-01 09:02:25 +0300

    Swagger UI eklendi ve API tamamlandı

--- PersonelYonetimApi/Program.cs
+++ PersonelYonetimApi/Program.cs
@@ -5,16 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
